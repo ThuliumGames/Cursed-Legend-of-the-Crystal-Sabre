b8a5263f6b7e7a41b34449ee8f32fe8481004015
ThuliumGames <thuliumgames@gmail.com>
2018-07-11 07:50:12 -0700

    All of the Files

---
+++ Super Duper Real Cursed/Assets/Scripts/GlobVars.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobVars : MonoBehaviour {
+	
+	public static bool Paused;
+	public static bool PlayerPause;
+}
---
+++ Super Duper Real Cursed/Assets/Scripts/UI/MenuCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCommand : MonoBehaviour {
+	
+	public string Type;
+	
+	void StartGame () {
+		
+	}
+	
+	void EndGame () {
+		
+	}
+	
+	void MoveItem () {
+		
+	}
+}
