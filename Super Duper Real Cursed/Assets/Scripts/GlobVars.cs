cf4f323c1170ba1c47d8495852b07c4afc0ffea1
Thulium Games <32959306+ThuliumGames@users.noreply.github.com>
2018-09-14 11:34:55 -0400

    Reset

--- Super Duper Real Cursed/Assets/Scripts/GlobVars.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GlobVars : MonoBehaviour {
-	
-	public static bool Paused;
-	public static bool PlayerPause;
-}
