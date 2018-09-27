41379108816a19672a5be1e30e741b94b8d0776d
Thulium Games <32959306+ThuliumGames@users.noreply.github.com>
2018-09-27 16:10:07 -0400

    Lots of things

--- Super Duper Real Cursed/Assets/Scripts/GlobVars.cs
+++ Super Duper Real Cursed/Assets/Scripts/GlobVars.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,4 +10,22 @@ public class GlobVars : MonoBehaviour {
 	//Pause The Movement of the player but NOT the Game
 	public static bool PlayerPause;
 	public static bool Reading;
+	//Quests
+	public string[] Quests;
+	public string[] DoneQuests;
+	
+	void Update () {
+		int i = 0;
+		foreach (string S in Quests) {
+			int a = 0;
+			foreach (string C in DoneQuests) {
+				if (S == C) {
+					Quests[i] = Quests[Quests.Length - 1];
+					Array.Resize(ref Quests, Quests.Length - 1);
+				}
+				++a;
+			}
+			++i;
+		}
+	}
 }
--- Super Duper Real Cursed/Assets/Scripts/Player/CamControl.cs
+++ Super Duper Real Cursed/Assets/Scripts/Player/CamControl.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CamControl : MonoBehaviour {
 	
 	public Transform ObjToFollow;
+	float UpDown;
+	public float Max;
+	public float Min;
 	public float Back;
 	public float Up;
 	
@@ -15,6 +18,9 @@ public class CamControl : MonoBehaviour {
 			transform.Rotate (0, SSInput.RHor[0]*100*Time.deltaTime, 0);
 			transform.Translate (0, Up, -Back);
 			transform.eulerAngles = new Vector3 (10, transform.eulerAngles.y, 0);
+			UpDown += -SSInput.RVert[0]*100*Time.deltaTime;
+			UpDown = Mathf.Clamp (UpDown, Min, Max);
+			transform.RotateAround (ObjToFollow.position+new Vector3 (0,1,0), transform.right, UpDown);
 		}
 	}
 }
