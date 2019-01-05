a0d4f02ad13d15ba15fbd9bd0f045add90669ad6
Thulium Games <32959306+ThuliumGames@users.noreply.github.com>
2019-01-04 21:08:53 -0500

    Bugs and Optimizing

---
+++ Cursed Modules/Assets/Global/Everything Code/Interactables.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactables : MonoBehaviour {
+	
+	public float Range;
+	public string ObjectText;
+	public string MessageName;
+	
+	void Update () {
+		
+		float Dist = 10000;
+		
+		float DFP = Vector3.Distance (transform.position, GameObject.Find("Player").transform.position);
+		
+		foreach (Interactables I in GameObject.FindObjectsOfType<Interactables>()) {
+			
+			DFP = Vector3.Distance (I.transform.position, GameObject.Find("Player").transform.position);
+			
+			if (DFP < Dist && DFP <= Range) {
+				Dist = DFP;
+			}
+		}
+		
+		DFP = Vector3.Distance (transform.position, GameObject.Find("Player").transform.position);
+		
+		if (Dist == DFP && DFP <= Range) {
+			GlobVars.NearInteractable = true;
+			GlobVars.InteractText = ObjectText;
+			if (SSInput.A[0] == "Pressed" && !GlobVars.PlayerPaused && !GlobVars.Reading) {
+				SendMessage(MessageName);
+			}
+		}
+	}
+}
--- Cursed Modules/Assets/M2 Inventory/Scripts/ItemObject.cs
+++ Cursed Modules/Assets/M2 Inventory/Scripts/ItemObject.cs
@@ -7,34 +7,8 @@ public class ItemObject : MonoBehaviour {
 	
 	public Item AssociatedItem;
 	
-	public float Range;
-	
-	void Update () {
-		
-		float Dist = 10000;
-		
-		float DFP = Vector3.Distance (transform.position, GameObject.Find("Player").transform.position);
-		
-		foreach (ItemObject I in GameObject.FindObjectsOfType<ItemObject>()) {
-			
-			DFP = Vector3.Distance (I.transform.position, GameObject.Find("Player").transform.position);
-			
-			if (DFP < Dist && DFP <= Range) {
-				Dist = DFP;
-			}
-		}
-		
-		DFP = Vector3.Distance (transform.position, GameObject.Find("Player").transform.position);
-		
-		if (Dist == DFP && DFP <= Range) {
-			
-			GlobVars.NearInteractable = true;
-			GlobVars.InteractText = "Pick Up";
-			
-			if (SSInput.A[0] == "Pressed" && !GlobVars.PlayerPaused && !GlobVars.Reading) {
-				GameObject.FindObjectOfType<Inventory>().Items.Add(AssociatedItem);
-				Destroy(this.gameObject);
-			}
-		}
+	void PickUpObject () {
+		GameObject.FindObjectOfType<Inventory>().Items.Add(AssociatedItem);
+		Destroy(this.gameObject);
 	}
 }
